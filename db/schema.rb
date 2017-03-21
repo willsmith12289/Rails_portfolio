c6385704947e30c17d40adc52cc49078dbd182f9
will <willsmith12289@gmail.com>
2017-03-21 10:06:46 -0400

    carrier wave working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321094756) do
+ActiveRecord::Schema.define(version: 20170321134030) do
 
   create_table "bootsy_image_galleries", force: :cascade do |t|
     t.integer  "bootsy_resource_id"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170321094756) do
     t.string   "url"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
